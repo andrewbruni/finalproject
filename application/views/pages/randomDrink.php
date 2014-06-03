3d2fe88816017d73482062764db11dd6afe68b3c
Skyler Meyer <skylermeyer@Skylers-MacBook-Pro.local>
2014-06-02 23:42:12 -0600

    Fixed the random button.

--- application/views/pages/randomDrink.php
+++ application/views/pages/randomDrink.php
@@ -6,14 +6,13 @@ $query = $this->db->query($sql) ;
 $results = array();
 foreach($query->result() as $row) 
 {
-	$results.push($row->recipe_id ); 	
+	array_push($results, $row->recipe_id ); 	
 }
-
 $randomDrink = rand(1, count($results));
 
 echo $randomDrink ; 
 
-$sql2 = "SELECT * FROM recipes WHERE recipe_id =".$results[$randomDrink]." " ; 
+$sql2 = "SELECT * FROM recipes WHERE recipe_id =".$results[$randomDrink]."" ; 
 $query2 = $this->db->query($sql2) ;
 
 foreach($query2->result() as $row2) 
@@ -21,6 +20,4 @@ foreach($query2->result() as $row2)
 	$name= $row2->name ; 	
 }
 redirect('view/?drink='.$name) ; 
-
-
 ?>
